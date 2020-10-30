75a31bfd2ddb66e27bd674d1e2ee0ed6b07e089f
toumari hikana <toumarihikana@gmail.com>
2020-10-31 03:52:00 +0900

    test move implementation
    直交座標系でいったんwasdで動けるようにした

--- Assets/Ardyna/Scripts/MovePlayer.cs
+++ Assets/Ardyna/Scripts/MovePlayer.cs
@@ -6,11 +6,54 @@ using UnityEngine.InputSystem;
 
 public class MovePlayer : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private AnimationCurve moveSpeedCurve;
+
+    [SerializeField] private bool isMoveing;
+
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    private float startTime;
+
+    [SerializeField]float progressTime;
+    float movedTime = 0.2f;
+
+
     public void MoveHorizontal(Vector2 direction)
     {
-        Debug.Log(direction);
+        if (!isMoveing)
+        {
+
+            //Debug.Log(direction);
+            startPosition = playerObject.transform.position;
+
+            endPosition.x = startPosition.x + (direction.x * 5f);
+            endPosition.y = startPosition.y;
+            endPosition.z = startPosition.z + (direction.y * 5f);
+
+            startTime = Time.time;
+            if (!(direction.x == 0f && direction.y == 0f))
+            {
+                isMoveing = true;
+            }
+            
+
+        }
     }
 
+    void Update()
+    {
+        if (isMoveing)
+        {
+            progressTime = Time.time - startTime;
+            float rate = progressTime / movedTime;
+            var curvedRate = moveSpeedCurve.Evaluate(rate);
+            playerObject.transform.position = Vector3.Lerp(startPosition, endPosition, curvedRate);
+            if (curvedRate == 1)
+            {
+                isMoveing = false;
+            }
+        }
+    }
 
 }

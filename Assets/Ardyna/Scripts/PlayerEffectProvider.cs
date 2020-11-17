42a744c6be0bc1002491cdcb3778d33f938243fc
toumari hikana <toumarihikana@gmail.com>
2020-11-18 06:55:12 +0900

    パンチエフェクトの実装
    パンチしたときに爆発エフェクトが起きるようにした。

---
+++ Assets/Ardyna/Scripts/PlayerEffectProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace com.AmberSyndrome.Ardyna
+{
+
+public class PlayerEffectProvider : MonoBehaviour
+{
+        [SerializeField] GameObject character;
+        void PunchEffectCall()
+        {
+            // インターフェースを継承しているコンポーネントのメソッドを呼び出し
+            ExecuteEvents.Execute<IMessageReciever>(
+                target: character, // 呼び出す対象のオブジェクト
+                eventData: null,  // イベントデータ（モジュール等の情報）
+                functor: (reciever, eventData) => reciever.PunchEffectGenerator()); // 操作
+        }
+    }
+}
